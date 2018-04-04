476a8d20b545df996040fb84f8374804f240f3ca
suzy baldwin <gbaldwin24@comcast.net>
2018-04-04 18:25:36 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,44 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-
   get '/' do
+    erb :index
+  end
+  get '/posts/new' do
+
+    erb :new
+  end
+  post '/posts' do
+    @post = Post.create(params)
+    erb :index
+  end
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :show
+  end
+  get '/posts/:id/edit' do
+      @post = Post.find(params[:id])
+    erb :edit
+  end
+  patch '/posts/:id' do
+
+    @post = Post.find(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    erb :show
+
 
   end
+  delete '/posts/:id/delete' do
+    
+    @post = Post.delete(params[:id])
+
+    erb :show
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 #Placeholder for a model
 
 class Post < ActiveRecord::Base
+  def change
+    create_table :posts do |t|
+      t.string :name
+      t.string :content
+    end
+  end
 end
